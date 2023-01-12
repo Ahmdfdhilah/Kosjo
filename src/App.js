3967d0185585126859c1e7222ac61d164d0b80fc
Ahmad_Fadillah <ahmad.fadilah0210@gmail.com>
2023-01-12 18:50:03 +0700

    changing browser router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React from'react';
-//rotas
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-//pages
+import {HashRouter, Routes, Route} from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
-//componentes
 import Navbar from './components/Navbar';
 import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <HashRouter>
         <Navbar />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/about' element={<About />} />
           <Route path='/contact' element={<Contact />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
       <Footer />
     </>
   );
